a9d7d24acab43f659db4dc366e8ed0c63931a2bf
youssef <youssefahmedabdelkader25@gmail.com>
2024-10-24 20:31:11 +0300

    update: the func to find the missing number in unsorted array

--- python/Find_the_Missing_Number.py
+++ python/Find_the_Missing_Number.py
@@ -1,8 +1,8 @@
 def find_missing_number(numbers: list, n: int) -> int:
     sum = []
-    for i in range(numbers[0], numbers[-1]):
+    for i in range(min(numbers), max(numbers)):
         if i not in numbers:
             sum.append(i)
     print(*sum)
 if __name__ == '__main__':
-    find_missing_number([1, 4, 5, 6, 7, 8, 9, 10], 10)
+    find_missing_number([7, 2, 4, 1, 8, 10, 9, 6], 10)
